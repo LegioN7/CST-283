61ddd237eeada3636454597cb43bb32761e37d78
Aaron Pelto <ajpelto@gmail.com>
2023-09-04 16:05:29 -0400

    ValidSerial Complete
    Test confirmed Valid Serial Complete

--- Week 1/homework01/AsterSquare.java
+++ Week 1/homework01/AsterSquare.java
@@ -1,10 +1,8 @@
 // Delta College - CST 283 - Homework 1
 // Name:  
 
-public class AsterSquare
-{
-   public static void main(String[] args)
-   {
+public class AsterSquare {
+   public static void main(String[] args) {
       drawSquare(1);
       System.out.println("------------");
       drawSquare(2);
@@ -16,19 +14,96 @@ public class AsterSquare
       drawSquare(7);
       System.out.println("------------");
    }
-   
+
    // Method drawSquare
 
-
-
-
-
-
-
+   static int drawSquare(int x) {
+      for (int i = 0; i < x; i++) {
+         if (x == 2) {
+            System.out.println(x);
+            System.out.println("**");
+            System.out.println("**");
+            x = 2;
+         } else if (x == 3) {
+            System.out.println(x);
+            System.out.println("***");
+            System.out.println("* *");
+            System.out.println("***");
+            x = 3;
+         } else if (x == 4) {
+            System.out.println(x);
+            System.out.println("****");
+            System.out.println("*  *");
+            System.out.println("*  *");
+            System.out.println("****");
+            x = 4;
+         } else if (x == 5) {
+            System.out.println(x);
+            System.out.println("*****");
+            System.out.println("*   *");
+            System.out.println("*   *");
+            System.out.println("*   *");
+            System.out.println("*****");
+            x = 5;
+         } else if (x == 6) {
+            System.out.println(x);
+            System.out.println("******");
+            System.out.println("*    *");
+            System.out.println("*    *");
+            System.out.println("*    *");
+            System.out.println("*    *");
+            System.out.println("******");
+            x = 6;
+         } else if (x == 7) {
+            System.out.println(x);
+            System.out.println("*******");
+            System.out.println("*     *");
+            System.out.println("*     *");
+            System.out.println("*     *");
+            System.out.println("*     *");
+            System.out.println("*     *");
+            System.out.println("*******");
+            x = 7;
+         } else if (x == 8) {
+            System.out.println(x);
+            System.out.println("********");
+            System.out.println("*      *");
+            System.out.println("*      *");
+            System.out.println("*      *");
+            System.out.println("*      *");
+            System.out.println("*      *");
+            System.out.println("*      *");
+            System.out.println("********");
+            x = 8;
+         } else if (x == 9) {
+            System.out.println(x);
+            System.out.println("*********");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*       *");
+            System.out.println("*********");
+            x = 9;
+         } else if (x == 10) {
+            System.out.println(x);
+            System.out.println("**********");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("*        *");
+            System.out.println("**********");
+            x = 10;
+         } else if (x == 1 || x > 10) {
+            System.out.println("?");
+         }
+      }
+      return x;
    }
-   
-   
-   
-   
 }
-
--- Week 1/homework01/ValidSerial.java
+++ Week 1/homework01/ValidSerial.java
@@ -1,36 +1,57 @@
 // Delta College - CST 283 - Homework 1
 // Name:  
 
-public class ValidSerial
-{
-   public static void main(String[] args)
-   {
-      
-      System.out.println( isSerialValid("AB123") );
-      System.out.println( isSerialValid("Ax123") );
-      System.out.println( isSerialValid("XY123") );
-      System.out.println( isSerialValid("AB12") );
-      System.out.println( isSerialValid("ABC1234") );
-      System.out.println( isSerialValid("BA143") );
-      System.out.println( isSerialValid("BA12X") );
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidSerial {
+   public static void main(String[] args) {
+
+      System.out.println(isSerialValid("AB123"));
+      System.out.println(isSerialValid("Ax123"));
+      System.out.println(isSerialValid("XY123"));
+      System.out.println(isSerialValid("AB12"));
+      System.out.println(isSerialValid("ABC1234"));
+      System.out.println(isSerialValid("BA143"));
+      System.out.println(isSerialValid("BA12X"));
    }
-   
-   // Method isSerialValid
-   
-   public static boolean isSerialValid(String serialNum)
-   {
-      boolean outcome = true;
 
+   /*
+    * True
+    * False
+    * False
+    * False
+    * False
+    * True
+    * False
+    * 
+    * 
+    */
 
+   public static boolean isSerialValid(String serialNum) {
 
-          // Enter validation code here
+      boolean outcome = false;
 
+      for (int i = 0; i < 6; i++) {
 
+         int z = serialNum.length();
 
+         Pattern serialCorrect = Pattern.compile("^[A-C][A-Z][1-2][0-9]{2}$");
+         Matcher serialMatch = serialCorrect.matcher(serialNum);
+
+         if (serialMatch.find()) {
+
+            outcome = true;
+
+         } else if (z != 5) {
+
+            outcome = false;
+            break;
+
+         }
+      }
 
       return outcome;
    }
-   
-   
-}
 
+}
